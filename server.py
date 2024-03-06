a8aebef1e239a06254592696bb552714d7db0b33
Omer Spalter <omers@nakairobotics.com>
2024-03-06 13:20:13 +0200

    add camera

--- server.py
+++ server.py
@@ -1,70 +1,94 @@
 import socketio
 import time
-import eventlet
+import gevent
+from flask import Flask, Response
+from flask_socketio import SocketIO
+from picamera import PiCamera
+from io import BytesIO
+from camera_output import CameraOutput
 
-# create a Socket.IO server
-sio = socketio.Server(cors_allowed_origins='*')
-
-# create a WSGI application
-app = socketio.WSGIApp(sio)
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins='*', async_mode='gevent')
 
 def send_status(parachute_armed, parachute_deployed):
-    sio.emit('status', { 'parachuteArmed': parachute_armed, 'parachuteDeployed': parachute_deployed})
+    socketio.emit('status', { 'parachuteArmed': parachute_armed, 'parachuteDeployed': parachute_deployed})
 
 def send_rocket_data(altitude):
-    sio.emit('rocket-data', { 'timestamp': time.time(), 'altitude': altitude})
+    socketio.emit('rocket-data', { 'timestamp': time.time(), 'altitude': altitude})
     
 # Event handler for new connections
-@sio.event
-def connect(sid, environ):
-    print('connect ', sid)
+@socketio.event
+def connect():
+    print('Client connected')
 
 # Event handler for messages
-@sio.event
+@socketio.event
 def message(sid, data):
     print('message ', data)
-    sio.send(sid, f"Reply: {data}")
+    socketio.send(sid, f"Reply: {data}")
 
 # Event handler for disconnections
-@sio.event
-def disconnect(sid):
-    print('disconnect ', sid)
+@socketio.event
+def disconnect():
+    print('client disconnected')
 
-@sio.on('arm-parachute')
-def arm_parachute(sid):
+@socketio.on('arm-parachute')
+def arm_parachute():
     print('arm-parachute')
 
     send_status(True, False)
 
-@sio.on('disarm-parachute')
-def arm_parachute(sid):
+@socketio.on('disarm-parachute')
+def arm_parachute():
     print('disarm-parachute')
 
     send_status(False, False)
 
-@sio.on('reset-parachute')
-def arm_parachute(sid):
+@socketio.on('reset-parachute')
+def arm_parachute():
     print('reset-parachute')
 
     send_status(False, False)
 
-@sio.on('deploy-parachute')
-def arm_parachute(sid):
+@socketio.on('deploy-parachute')
+def arm_parachute():
     print('deploy-parachute')
 
     send_status(True, True)
 
-@sio.on('launch')
-def arm_parachute(sid):
+@socketio.on('launch')
+def arm_parachute():
     print('launch')
+
+def record_video():
+    camera = PiCamera()
+    camera.resolution = (640, 480)
+    camera.start_recording(f'video{time.time()}.h264')
+    camera.wait_recording(60)
+    camera.stop_recording()
+
+def generate_camera_stream(output):
+    while True:
+        with output.condition:
+            output.condition.wait()
+            frame = output.frame
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/stream')
+def video_feed():
+    return Response(generate_camera_stream(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 def read_and_send_data():
     while True:
         send_rocket_data(1)
-        eventlet.sleep(1) # Send data every 1 second
-
+        gevent.sleep(1) # Send data every 1 second, change this
 
 if __name__ == '__main__':
-    eventlet.spawn(read_and_send_data)
+    output = CameraOutput(f'video-{time.time()}.h264', 'mjpeg')
 
-    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
+    gevent.spawn(read_and_send_data)
+    gevent.spawn(record_video)
+
+    socketio.run(app, port=5000, host='0.0.0.0', debug=False)
