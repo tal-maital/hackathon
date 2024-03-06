64ddae82c08cae10e963c41b32e4a3cbf2c7d426
Omer Spalter <omers@nakairobotics.com>
2024-03-06 17:38:15 +0200

    update

--- drivers/lps22.py
+++ drivers/lps22.py
@@ -4,7 +4,7 @@
 # Author: shaoziyang (shaoziyang@micropython.org.cn)
 # v1.0 2016.4
 # v2.0 2019.7
-import board
+from machine import I2C
 
 LPS22_CTRL_REG1    = 0x10
 LPS22_CTRL_REG2    = 0x11
@@ -15,7 +15,7 @@ LPS22_PRESS_OUT_L  = 0x29
 
 class LPS22():
     def __init__(self, addr = 0x5D):
-        self.i2c = board.I2C()
+        self.i2c = I2C(1)
         self.addr = addr
         self.tb = bytearray(1)
         self.rb = bytearray(1)
