357763c8bc3296fd69abe6d079af2293ddb382a9
talm <talm@nakairobotics.com>
2024-03-09 23:35:35 +0200

    fixed imu address

--- drivers/altimu10v6.py
+++ drivers/altimu10v6.py
@@ -5,7 +5,7 @@ from adafruit_lsm6ds.lsm6dsox import LSM6DSOX
 class Altimu10V6():
     def __init__(self):
         i2c = board.I2C() 
-        self.accelerometer = LSM6DSOX(i2c)
+        self.accelerometer = LSM6DSOX(i2c, address=0x6b)
 
     def calibrate(self):
         self.accelerometer.calibrate()
