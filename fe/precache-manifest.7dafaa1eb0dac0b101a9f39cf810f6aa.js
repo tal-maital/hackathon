974fcd55d54a16fad83fbeb2281c0d3b68fbc47e
Tal <talm@nakairobotics.com>
2024-03-10 06:35:08 +0200

    omer fixes

---
+++ fe/precache-manifest.7dafaa1eb0dac0b101a9f39cf810f6aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "169a7ccd936d395497816561b4739956",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba2bee18803a48e443da",
+    "url": "/static/css/2.0063b37b.chunk.css"
+  },
+  {
+    "revision": "101f86e6a3269bc30a4e",
+    "url": "/static/css/main.fb81763c.chunk.css"
+  },
+  {
+    "revision": "ba2bee18803a48e443da",
+    "url": "/static/js/2.38071385.chunk.js"
+  },
+  {
+    "revision": "101f86e6a3269bc30a4e",
+    "url": "/static/js/main.ecbbe388.chunk.js"
+  },
+  {
+    "revision": "ebd9050f247c8c7a7ff1",
+    "url": "/static/js/runtime~main.82429862.js"
+  },
+  {
+    "revision": "fcac99b0ab3f9ff11257b60d84bdbd6b",
+    "url": "/static/media/white-logo.fcac99b0.png"
+  }
+]);
